ec96bf5d5051ad6c11c6d98703aa118f826591cd
Nirmal Garg <nirmalgarg2006@gmail.com>
2020-06-23 08:20:39 +0530

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            mygrid.DataContext = this;
+        }
+
+        int i = 1;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sName = (i++).ToString();
+        }
+
+        protected void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        private string sname;
+
+        public string sName
+        {
+            get { return sname; }
+            set
+            {
+                if(value != sname)
+                {
+                    sname = value;
+                    OnPropertyChanged("sName");
+                }
+            }
+        }
+    }
+}
